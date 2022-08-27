ca5798ed9f3f1ca461e4c46059a39e90f5edbd31
Pramod Naik <pramodnaik221@gmail.com>
2022-08-27 15:09:58 +0530

    Add project files.

---
+++ PurchaseBookApp/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PurchaseBookApp.Models;
+
+[assembly: FunctionsStartup(typeof(PurchaseBookApp.Startup))]
+namespace PurchaseBookApp
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            string SqlConnection = "Data Source=CTSDOTNET664;Initial Catalog=DigitalBookManagement;Persist Security Info=True;User ID=sa;Password=pass@word1;";
+            builder.Services.AddDbContext<DigitalBookManagementContext>(
+                options => options.UseSqlServer(SqlConnection));
+        }
+    }
+}
